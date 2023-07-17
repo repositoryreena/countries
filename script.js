ad082fa0d6dddf93ff330053f94ec14bfab0fad0
Reena Thomas <rtthomas@ucsc.edu>
2023-07-16 18:52:43 -0700

    toggle theme

--- script.js
+++ script.js
@@ -1,83 +1,96 @@
-// Function to fetch country data from data.json
-function fetchCountryData() {
-    return fetch('data.json')
-      .then(response => response.json())
-      .catch(error => {
-        console.error('Error fetching country data:', error);
-        return [];
+// Get DOM elements
+// Get DOM elements
+const themeSwitch = document.getElementById('theme-switch');
+const searchInput = document.getElementById('search-input');
+const regionSelect = document.getElementById('region-select');
+const countryList = document.getElementById('country-list');
+
+// Function to toggle dark/light theme
+function toggleTheme() {
+  document.body.classList.toggle('dark-theme');
+}
+
+// Event listener for theme switch button
+themeSwitch.addEventListener('click', toggleTheme);
+
+// ... (rest of the code remains unchanged)
+
+
+// Fetch country data from data.json
+fetch('data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    // Display all countries on the homepage
+    function displayCountries(countries) {
+      countryList.innerHTML = '';
+      countries.forEach((country) => {
+        const countryCard = createCountryCard(country);
+        countryList.appendChild(countryCard);
       });
-  }
-  
-  // Function to create a country card for each country
-  function createCountryCards(countries) {
-    const countryListContainer = document.getElementById('country-list');
-  
-    countries.forEach(country => {
-      // Create a div element for the country card
+    }
+
+    // Create a country card element
+    function createCountryCard(country) {
+      const { name, population, region, capital, flags } = country;
+
       const countryCard = document.createElement('div');
       countryCard.classList.add('country-card');
-  
-      // Populate the country card with country information
-      countryCard.innerHTML = `
-        <img src="${country.flags.svg}" alt="${country.name} Flag">
-        <h3>${country.name}</h3>
-        <p>Population: ${country.population.toLocaleString()}</p>
-        <p>Region: ${country.region}</p>
-        <p>Capital: ${country.capital}</p>
-      `;
-  
-      // Add click event listener to show detailed country information on click
-      countryCard.addEventListener('click', () => showCountryDetails(country));
-  
-      // Append the country card to the container element
-      countryListContainer.appendChild(countryCard);
+
+      const flagImg = document.createElement('img');
+      flagImg.src = flags.svg;
+      flagImg.alt = `Flag of ${name}`;
+
+      const countryName = document.createElement('h3');
+      countryName.textContent = name;
+
+      const countryPopulation = document.createElement('p');
+      countryPopulation.textContent = `Population: ${population.toLocaleString()}`;
+
+      const countryRegion = document.createElement('p');
+      countryRegion.textContent = `Region: ${region}`;
+
+      const countryCapital = document.createElement('p');
+      countryCapital.textContent = `Capital: ${capital}`;
+
+      countryCard.appendChild(flagImg);
+      countryCard.appendChild(countryName);
+      countryCard.appendChild(countryPopulation);
+      countryCard.appendChild(countryRegion);
+      countryCard.appendChild(countryCapital);
+
+      return countryCard;
+    }
+
+    // Filter countries by search input
+    function searchCountries(searchTerm) {
+      const filteredCountries = data.filter((country) =>
+        country.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      displayCountries(filteredCountries);
+    }
+
+    // Event listener for search input
+    searchInput.addEventListener('input', (event) => {
+      const searchTerm = event.target.value.trim();
+      searchCountries(searchTerm);
     });
-  }
-  
-  // Function to show detailed country information
-  function showCountryDetails(country) {
-    // Replace the content of the 'country-details' section with detailed information about the selected country
-    const countryDetailsContainer = document.getElementById('country-details');
-    countryDetailsContainer.innerHTML = `
-      <button id="back-button" onclick="hideCountryDetails()">Back</button>
-      <div class="country-info">
-        <div class="flag">
-          <img src="${country.flags.svg}" alt="${country.name} Flag" id="flag-image">
-        </div>
-        <div class="details">
-          <h2 id="country-name">${country.name}</h2>
-          <p><strong>Native Name:</strong> <span id="native-name">${country.nativeName}</span></p>
-          <p><strong>Population:</strong> <span id="population">${country.population.toLocaleString()}</span></p>
-          <p><strong>Region:</strong> <span id="region">${country.region}</span></p>
-          <p><strong>Sub Region:</strong> <span id="sub-region">${country.subregion}</span></p>
-          <!-- Add more details here -->
-        </div>
-      </div>
-      <!-- Border Countries -->
-      <div class="border-countries">
-        ${country.borders.map(border => `<button>${border}</button>`).join('')}
-      </div>
-    `;
-  
-    // Display the 'country-details' section and hide the 'home' section
-    const homeSection = document.querySelector('.home');
-    const countryDetailsSection = document.querySelector('.country-details');
-    homeSection.style.display = 'none';
-    countryDetailsSection.style.display = 'block';
-  }
-  
-  // Function to hide the detailed country information and show the 'home' section
-  function hideCountryDetails() {
-    const homeSection = document.querySelector('.home');
-    const countryDetailsSection = document.querySelector('.country-details');
-    homeSection.style.display = 'block';
-    countryDetailsSection.style.display = 'none';
-  }
-  
-  // Fetch the country data from data.json and generate country cards when the page loads
-  document.addEventListener('DOMContentLoaded', () => {
-    fetchCountryData()
-      .then(data => createCountryCards(data))
-      .catch(error => console.error('Error fetching country data:', error));
+
+    // Filter countries by region
+    function filterByRegion(region) {
+      if (region === 'all') {
+        displayCountries(data);
+      } else {
+        const filteredCountries = data.filter((country) => country.region === region);
+        displayCountries(filteredCountries);
+      }
+    }
+
+    // Event listener for region select
+    regionSelect.addEventListener('change', (event) => {
+      const selectedRegion = event.target.value;
+      filterByRegion(selectedRegion);
+    });
+
+    // Initial display of all countries
+    displayCountries(data);
   });
-  
